2ae25fbd3645ca994ab3d24d019971b68be6721e
mohan-81 <mohanthambabattula@gmail.com>
2026-02-28 15:14:19 +0530

    fix: error fix in singup page to store new profiles

--- identity_server.py
+++ identity_server.py
@@ -71,7 +71,7 @@ app = Flask(__name__)
 CORS(
     app,
     supports_credentials=True,
-    origins=["http://localhost:3000"]
+    origins=["http://localhost:3000", "http://127.0.0.1:3000"]
 )
 
 from flask import g
